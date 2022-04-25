5e9e08090771cfebfde71cb69def3d0257fb6353
何渊 <hzheyuan@corp.netease.com>
2022-04-25 23:59:08 +0800

    feat(src): update up for LIS and LCS
    update up for LIS and LCS

--- src/DP/main.cpp
+++ src/DP/main.cpp
@@ -10,12 +10,14 @@
 #include <vector>
 using namespace std;
 
-vector<int> a = {2, 4, 3, 1, 5, 6, 7};
-vector<int> b = {2, 4, 6, 7, 3, 7, 5};
+vector<int> a = {2, 4, 3, 3, 1, 5, 6, 7};
+vector<int> c = {7, 7, 7, 7, 7, 7, 7, 7};
+
 int n = a.size();
-int mem[7];
+int mem[8];
 
 // LIS(Longest Increasing Subsequence) promble
+// 最长上升（递增）子序列问题
 
 /** solution1: 记忆话化搜索 */
 int dfs(int i) {
@@ -32,7 +34,7 @@ int dfs(int i) {
 // f(i)为以第i个元素为结尾的最长上升子序列长度
 // O(n^2)
 // dp_i = max{dp_j + 1} (1 <= j < i and a_j < a_i);/
-int LIS(vector<int> &a) {
+int LIS(vector<int> a) {
   int n = a.size();
   int ans = 1;
   vector<int> f(n + 1, 1);
@@ -47,50 +49,41 @@ int LIS(vector<int> &a) {
   return ans;
 };
 
-// int LIS2(vector<int> &a) {
-//   int n = a.size();
-//   vector<int> f(n + 1, INT_MAX);
-//   f[1] = a[0];
-//   int len = 1;
-//   for (int i = 2; i <= n; ++i) {
-//     int l = 0, r = n, mid;
-//     if (a[i] > f[len]) f[++len] = a[i];
-//     else {
-//       while (l < r) {
-//         mid = (l + r) / 2;
-//         if (f[mid] > a[i])
-//           r = mid;
-//         else
-//           l = mid + 1;
-//       };
-//     };
-//     f[l] = min(a[i], f[l]);
-//   };
-//   return len;
-// };
 
-/** solution2: dp optimal */
-// dp[k]表示长度为k的不下降子序列末尾元素的最小值/
-int LIS3(vector<int> &a) {
-  int n = a.size();
-  vector<int> dp(n, INT_MAX);
-  for (int i = 0; i < n; ++i) {
-    auto iter = upper_bound(dp.begin(), dp.end(), a[i]);
-    *iter = a[i];
+/** 最长上升子序列 二分优化 */
+int LISBIN(vector<int> a) {
+  // dp[i]表示长度为i的最长上升子序列的末尾的最小值
+  vector<int> dp;
+  for(auto n : a) {
+    auto iter = lower_bound(dp.begin(), dp.end(), n);
+    if(iter != dp.end()) *iter = n;
+    else dp.push_back(n);
   }
-  int ans = 0;
-  while (dp[ans] != INT_MAX) ++ans;
-  return ans;
+  return dp.size();
+}
+
+// 最长严格递增子序列
+// dp[k]表示长度为k的不下降子序列末尾元素的最小值/
+int LIS3(vector<int> a) {
+  vector<int> dp;
+  for(auto n : a) {
+    auto iter = upper_bound(dp.begin(), dp.end(), n);
+    if(iter != dp.end()) *iter = n;
+    else dp.push_back(n);
+  }
+  return dp.size();
 }
 
 // LCS Longest Common Subsequence
-int LCS(vector<int> &a, vector<int> &b) {
+string str1 = "abcde", str2 = "ace";
+int LCS(string &a, string &b) {
   int len1 = a.size(), len2 = b.size();
   vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
   // dp[i][j]代表a的前i位，b的前j位的LCS长度
-  for (int i = 1; i < len1; ++i) {
-    for (int j = 1; j < i; ++j) {
-      if (a[i] == a[j])
+  // 这里i,j从1开始，而字符串中的位置则是i-1
+  for (int i = 1; i <= len1; ++i) {
+    for (int j = 1; j <= len2; ++j) {
+      if (a[i-1] == a[j-1])
         dp[i][j] = dp[i - 1][j - 1] + 1;
       else
         dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
@@ -101,16 +94,11 @@ int LCS(vector<int> &a, vector<int> &b) {
 
 int main() {
   memset(mem, -1, sizeof(mem));
-  int res1 = dfs(n-1);
-  cout << "LIS dfs " << res1 << endl;
-
-  int res2 = LIS(a);
-  cout << "LIS 1 " << res2 << endl;
-
-  int res3 = LIS3(a);
-  cout << "LIS 2 " << res3 << endl;
-
-  int lcs = LCS(a, b);
-  cout << "LCS " << res1 << endl;
+  cout << "LIS 记忆化搜索: " << dfs(n-1) << endl;
+  cout << "LIS dp: " << LIS(a) << endl;
+  cout << "LIS 二分优化: " << LISBIN(a) << endl;
+  cout << "LIS 二分优化: " << LISBIN(c) << endl;
+  cout << "LIS 不下降最长子序列: " << LIS3(a) << endl;
+  cout << "LCS 最长公共子序列: " << LCS(str1, str2) << endl;
   return 0;
 }
